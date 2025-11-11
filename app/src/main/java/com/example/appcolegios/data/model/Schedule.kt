2efa04beb022e1c870c2099abc7be8c48be73df8
Juanherndia <145281367+Juanherndia@users.noreply.github.com>
2025-11-11 09:32:19 -0500

    agregar grupos a estudiantes

--- app/src/main/java/com/example/appcolegios/data/model/Schedule.kt
+++ app/src/main/java/com/example/appcolegios/data/model/Schedule.kt
@@ -1,11 +1,10 @@
 package com.example.appcolegios.data.model
 
 data class ClassSession(
-    val dayOfWeek: Int, // 1 for Monday, 7 for Sunday
-    val subject: String,
-    val teacher: String,
-    val startTime: String, // "HH:mm"
-    val endTime: String, // "HH:mm"
-    val classroom: String
+    var dayOfWeek: Int = 1, // 1 for Monday, 7 for Sunday; default 1
+    var subject: String = "",
+    var teacher: String = "",
+    var startTime: String = "", // "HH:mm"
+    var endTime: String = "", // "HH:mm"
+    var classroom: String = ""
 )
-
